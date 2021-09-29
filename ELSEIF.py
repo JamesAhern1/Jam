387765d5c69809444cb7ea187a94a52be64c0f2d
JamesAhern1 <90275735+JamesAhern1@users.noreply.github.com>
2021-09-29 11:01:44 +0100

    Add files via upload

---
+++ ELSEIF.py
@@ -0,0 +1,10 @@
+name = "bob"
+age = 10
+if name == 'Alice':
+    print ("Hi Alice")
+elif age < 12:
+    print("You are not Alice.")
+elif age > 2000:
+    print("Boomer")
+elif age > 100:
+    print("Granny")
